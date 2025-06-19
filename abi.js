526580a31a3ab59a4a84b1731f8946e10a454168
jin1963 <kaojino89@gmail.com>
2025-06-19 16:49:57 +0700

    Add files via upload

---
+++ abi.js
@@ -0,0 +1,2 @@
+
+const stakingABI = [{"inputs":[{"internalType":"address","name":"_token","type":"address"}],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[],"name":"CLAIM_INTERVAL","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}];
